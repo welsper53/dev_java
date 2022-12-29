2954bfd9ec1c995013afeb8ddef8ef6518533cd4
welsper53 <welsper53@gmail.com>
2022-12-29 17:06:59 +0900

    2022-12-29 네트워크

--- app/src/main/java/dev_java/week5/d221229/TomcatServer.java
+++ app/src/main/java/dev_java/week5/d221229/TomcatServer.java
@@ -1,7 +1,34 @@
 package dev_java.week5.d221229;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class TomcatServer {
     public static void main(String[] args) {
         String url = "http://192.168.10.72:9000/index.html";
+        try {
+            // 예외가 발생할 가능성이 있는 코드
+            URL myURL = new URL(url);
+            HttpURLConnection con = (HttpURLConnection)myURL.openConnection();
+            int responseCode = con.getResponseCode(); // 프로토콜출력 -> 200, 204, 404, 500
+            // System.out.println("응답코드 : " + responseCode);
+            // System.out.println(5/0);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            while((line=br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            System.out.println(sb.toString());
+
+        } catch (Exception e) {
+            // TODO: handle exception
+
+        }
     }
 }
